01f1baa8696ed5ea7b66f0f21c11871f23f5e235
Steve Korshakov <steve@korshakov.com>
2024-05-18 06:15:31 +0000

    fix: fix hubconf

--- hubconf.py
+++ hubconf.py
@@ -3,7 +3,8 @@ dependencies = ['torch', 'torchaudio']
 def flow():
     import torch
     from supervoice_flow.model import AudioFlow
+    model = AudioFlow()
     checkpoint = torch.hub.load_state_dict_from_url("https://shared.korshakov.com/models/supervoice_flow.pt")
     model.load_state_dict(checkpoint['model'])
-    return AudioFlow
+    return model
             
